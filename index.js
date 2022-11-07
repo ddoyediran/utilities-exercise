59b7bcea935b82673ac31446f37f3f0fb33751fc
ddoyediran <ddoyediran@gmail.com>
2022-11-07 23:25:53 +0100

    final solution

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
+const { largeNumber, cut3 } = require("./utilities/firstFunction");
+const { sum, concat } = require("./utilities/secondFunction");
 
+console.log(largeNumber([1, 2, 3, 4])); // 4
+
+console.log(cut3([1, 2, 3, 4])); // [ 1, 2, 4 ]
+
+console.log(sum([1, 2, 3, 4])); // 10
+
+console.log(concat([1, 2, 3, 4], [5, 6, 7, 8])); // [1, 2, 3, 4, 5, 6, 7, 8]
